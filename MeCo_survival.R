ef74aa2ce8ce99889374f60dd2f47bfd191944d1
albptt <alberto.pettenella@mail.polimi.it>
2022-05-17 10:09:31 +0200

    ph assumptions verified

--- MeCo_survival.R
+++ MeCo_survival.R
@@ -18,6 +18,7 @@ for (i in 1:length(sequenced_patients)){
   else
     surv_data$time[i] <- time_to_death[i]
 }
+
 #status
 surv_data$status <- as.numeric(as.factor(pheno_data$vital_status.demographic)) # 1 = patient alive, 2 = patient dead
 #metastasis
@@ -45,6 +46,10 @@ surv_data$MeCo_Pro <- MecoScorePro$MeSc
 # laterality
 surv_data$laterality <- as.factor(pheno_data$laterality)
 
+# remove time=0
+which(surv_data$time==0)
+surv_data <- surv_data[-c(220,302,429,440),]
+
 # Perform survival analysis
 # fit the Kaplan-Meier estimator and plot the curve considering no stratification
 fit <- survfit(Surv(time, status==2) ~ 1, data = surv_data)
@@ -63,7 +68,7 @@ legend('topright', legend=c('Male','Female'), lty=c(1,1), col=c("dodgerblue2", "
 grid()
 # perform log rank test
 survdiff(Surv(time, status) ~ gender, data = surv_data) 
-# p=0.6 there is strong statistical evidence against a difference between the survival of the two sexes
+# p=0.7 there is strong statistical evidence against a difference between the survival of the two sexes
 
 # investigate survival based on laterality of the tumor
 fit.lat <- survfit(Surv(time, status==2) ~ laterality, data = surv_data)
@@ -75,7 +80,7 @@ legend('topright', legend=c('Bilateral','Left','Right'), lty=c(1,1), col=c('red'
 grid()
 # perform log rank test
 survdiff(Surv(time, status) ~ laterality, data = surv_data) 
-# p=0.06 statistical evidence is not sufficient to state a difference in the survival of patients based on laterality of the tumor
+# p=0.08 statistical evidence is not sufficient to state a difference in the survival of patients based on laterality of the tumor
 
 # investigate survival based on tumor stage
 fit.stage <- survfit(Surv(time, status==2) ~ stage, data = surv_data)
@@ -98,14 +103,14 @@ legend('topright', legend=c('NO','YES'), lty=c(1,1), col=c("green", "red"))
 grid()
 # perform log rank test
 survdiff(Surv(time, status) ~ metastasis, data = surv_data) 
-# p=6e-06 there is strong statistical evidence for a difference in the survival of patients with and without metastasis
+# p=7e-06 there is strong statistical evidence for a difference in the survival of patients with and without metastasis
 
 # investigate survival by age
 # in order to do so, visualize the distribution of the variable to establish a cut-off point so to categorize the data
 hist(surv_data$age, xlab='Age', main='Histogram of Age in ccRCC data')
 summary(surv_data$age)
 # consider the median as a cut-off
-surv_data$agecat <- cut(surv_data$age, breaks=c(0,61,Inf), label=c('young','old'))
+surv_data$agecat <- cut(surv_data$age, breaks=c(0,60,Inf), label=c('young','old'))
 fit.agecat <- survfit(Surv(time,status)~agecat, data=surv_data)
 summary(fit.agecat)
 print(fit.agecat)
@@ -114,7 +119,7 @@ legend('topright', legend=c('Young','Old'), lty=c(1,1), col=c("blue", "green"))
 grid()
 # perform Log-Rank test
 survdiff(Surv(time,status)~agecat, data=surv_data)
-# p=2e-04 there is strong statistical evidence for a difference in the survival of younger and older patients 
+# p=4e-04 there is strong statistical evidence for a difference in the survival of younger and older patients 
 
 # investigate survival by age and sex 
 fit.agecat.sex <- survfit(Surv(time,status)~agecat+gender, data=surv_data)
@@ -137,7 +142,7 @@ survdiff(Surv(time,status)~gender, data=surv_data[surv_data$agecat=='old'& surv_
 hist(surv_data$MeCo)
 summary(surv_data$MeCo)
 # cut-off=median(MeCo)=0.19318
-surv_data$mecocat <- cut(surv_data$MeCo, breaks=c(-Inf,0.19318,Inf), labels=c('Low MeCo','High MeCo'))
+surv_data$mecocat <- cut(surv_data$MeCo, breaks=c(-Inf,0.19343,Inf), labels=c('Low MeCo','High MeCo'))
 fit.mecocat <- survfit(Surv(time,status)~mecocat, data=surv_data)
 summary(fit.mecocat)
 print(fit.mecocat)
@@ -183,13 +188,13 @@ legend('topright', legend=c('Low MeCo','High MeCo'), lty=c(1,1), col=c("blue", "
 grid()
 # perform Log-Rank test
 survival::survdiff(survival::Surv(time,status)~mecocat_ant, data=surv_data)
-# p=0.7 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
+# p=0.6 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
 
 # investigate MeCo_Ch
 hist(surv_data$MeCo_Ch)
 summary(surv_data$MeCo_Ch)
 # cut-off=median(MeCo_Ch)=-0.4313
-surv_data$mecocat_Ch <- cut(surv_data$MeCo_Ch, breaks=c(-Inf,-0.4313,Inf), labels=c('Low MeCo_Ch','High MeCo_Ch'))
+surv_data$mecocat_Ch <- cut(surv_data$MeCo_Ch, breaks=c(-Inf,-0.9699,Inf), labels=c('Low MeCo_Ch','High MeCo_Ch'))
 fit.mecocat_Ch <- survival::survfit(survival::Surv(time,status)~mecocat_Ch, data=surv_data)
 summary(fit.mecocat_Ch)
 print(fit.mecocat_Ch)
@@ -198,13 +203,13 @@ legend('topright', legend=c('Low MeCo','High MeCo'), lty=c(1,1), col=c("blue", "
 grid()
 # perform Log-Rank test
 survival::survdiff(survival::Surv(time,status)~mecocat_Ch, data=surv_data)
-# p=0.8 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
+# p=0.6 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
 
 # investigate MeCo_ECM
 hist(surv_data$MeCo_ECM)
 summary(surv_data$MeCo_ECM)
 # cut-off=median(MeCo_Ch)=-0.2611
-surv_data$mecocat_ECM <- cut(surv_data$MeCo_ECM, breaks=c(-Inf,-0.2611,Inf), labels=c('Low MeCo_ECM','High MeCo_ECM'))
+surv_data$mecocat_ECM <- cut(surv_data$MeCo_ECM, breaks=c(-Inf,-0.2602,Inf), labels=c('Low MeCo_ECM','High MeCo_ECM'))
 fit.mecocat_ECM <- survival::survfit(survival::Surv(time,status)~mecocat_ECM, data=surv_data)
 summary(fit.mecocat_ECM)
 print(fit.mecocat_ECM)
@@ -219,7 +224,7 @@ survival::survdiff(survival::Surv(time,status)~mecocat_ECM, data=surv_data)
 hist(surv_data$MeCo_Inf)
 summary(surv_data$MeCo_Inf)
 # cut-off=median(MeCo_Ch)=0.6201
-surv_data$mecocat_Inf <- cut(surv_data$MeCo_Inf, breaks=c(-Inf,0.6201,Inf), labels=c('Low MeCo_Inf','High MeCo_Inf'))
+surv_data$mecocat_Inf <- cut(surv_data$MeCo_Inf, breaks=c(-Inf,0.6227,Inf), labels=c('Low MeCo_Inf','High MeCo_Inf'))
 fit.mecocat_Inf <- survival::survfit(survival::Surv(time,status)~mecocat_Inf, data=surv_data)
 summary(fit.mecocat_Inf)
 print(fit.mecocat_Inf)
@@ -228,13 +233,13 @@ legend('topright', legend=c('Low MeCo','High MeCo'), lty=c(1,1), col=c("blue", "
 grid()
 # perform Log-Rank test
 survival::survdiff(survival::Surv(time,status)~mecocat_Inf, data=surv_data)
-# p=0.6 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
+# p=0.7 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
 
 # investigate MeCo_Pro
 hist(surv_data$MeCo_Pro)
 summary(surv_data$MeCo_Pro)
 # cut-off=median(MeCo_Ch)=0.5060
-surv_data$mecocat_Pro <- cut(surv_data$MeCo_Pro, breaks=c(-Inf,0.5060,Inf), labels=c('Low MeCo_Pro','High MeCo_Pro'))
+surv_data$mecocat_Pro <- cut(surv_data$MeCo_Pro, breaks=c(-Inf,0.5066,Inf), labels=c('Low MeCo_Pro','High MeCo_Pro'))
 fit.mecocat_Pro <- survival::survfit(survival::Surv(time,status)~mecocat_Pro, data=surv_data)
 summary(fit.mecocat_Pro)
 print(fit.mecocat_Pro)
@@ -243,7 +248,7 @@ legend('topright', legend=c('Low MeCo','High MeCo'), lty=c(1,1), col=c("blue", "
 grid()
 # perform Log-Rank test
 survival::survdiff(survival::Surv(time,status)~mecocat_Pro, data=surv_data)
-# p=0.09 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
+# p=0.07 there is no statistical evidence of a difference in the survival of patients with higher and lower meco scores
 
 # In order to better evaluate the relationship between covariates and the role of quantitative predictors, such as MeCo scores
 # now build a multivariate Cox Proportional Hazard model 
@@ -297,4 +302,39 @@ cox.meco_Pro <- survival::coxph(survival::Surv(time, status)~MeCo_Pro, data=surv
 summary(cox.meco_Pro)
 print(cox.meco_Pro)
 ggforest(cox.meco_Pro, data=surv_data)
-# MeCo_Pro appers to be significant at a level of 0.0587
+# MeCo_Pro appers to be significant at a level of 0.0521
+
+# now verify the assumptions of the cox proportional hazard model 
+
+# firstly assess goodness of fit:
+# plot the martingale residuals and verify that they have mean=0 
+# residuals vs linear predictions
+ggcoxdiagnostics(cox.meco_Pro, type = "martingale",linear.predictions = T)
+# residuals vs observation id
+ggcoxdiagnostics(cox.meco_Pro, type = "martingale",linear.predictions = F)
+# the condition seems to hold
+
+# deviance residuals
+ggcoxdiagnostics(cox.meco_Pro, type = "deviance",linear.predictions = T)
+# mean remains close to zero, yet no simmetry is observed
+
+# now assess proportional hazards:
+# Schoenefeld residuals
+diag.ph <- cox.zph(cox.meco_Pro)
+ggcoxzph(diag.ph)
+# residuals are centered around zero and they do not show any particular pattern along time, thus the PH assumption seems to hold
+
+# now plot $log(-log(KM(t)))$ vs. $t$ or $log(t)$ and look for parallelism:
+plot(fit.mecocat_Pro, fun='cloglog', 
+     col=c("deeppink2","dodgerblue2"), lwd=2, lty=1,
+     ylab="log(-log(Survival Probability))")
+grid()
+legend('topleft', c("Meco Low", "Meco High"),
+       lty=c(1,1), lwd=c(2,2), col=c("deeppink2","dodgerblue2"))
+# curves seem to be parallel: PH assumption holds
+
+# finally, return pvalue of the sclaed Shoenfeld test:
+# +   $H_0$: Hazards are proportional
+# +   $H_1$: Hazards are NOT proportional
+diag.ph
+# p=0.95, so there is sotrn evidence of proportionality of hazards
